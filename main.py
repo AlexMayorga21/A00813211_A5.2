c00e95fa0456c6c9da710ceee4f9e0bd8a363cb4
Diego Mayorga <mayorgadiego21@gmail.com>
2026-02-15 17:27:28 -0600

    read files

--- main.py
+++ main.py
@@ -1,16 +1,52 @@
-# This is a sample Python script.
+"""
+Module for computing prices associated on catalog from files.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+This module provides functionality to calculate total amount of sales read from a json file, the catalog of
+product prices is also read from a json file.
+"""
+import sys
+import time
+import json
+
+def main():
+    """
+    Main function to calculate total amount of sales from a file.
+
+    Reads price catalog from a file specified as command line argument,
+    calculates the total amount of sales and writes results to file and console.
+    """
+    start_time = time.time()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+    # price catalog on argument 1
+    price_catalog_file_name = sys.argv[1]
+    # sales summary on argument 2
+    sales_summary_file_name = sys.argv[2]
 
 
-# Press the green button in the gutter to run the script.
+    # Open the file for reading
+    try:
+        with open(price_catalog_file_name, 'r', encoding='utf-8') as file:
+            price_catalog = json.load(file)
+    except FileNotFoundError:
+        print("Error price catalog file not found process cannot continue without it")
+        elapsed_time = time.time() - start_time
+        print(f"elapsed_time: {elapsed_time}")
+        return
+
+    try:
+        with open(sales_summary_file_name, 'r', encoding='utf-8') as file:
+            sales_summary = json.load(file)
+    except FileNotFoundError:
+        print("Sales summary file not found process cannot continue without it")
+        elapsed_time = time.time() - start_time
+        print(f"elapsed_time: {elapsed_time}")
+        return
+
+    print(price_catalog)
+
+    print(sales_summary)
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
